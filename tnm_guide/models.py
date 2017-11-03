534a0ee20bcdbc7e748e5e6ec867b122443eba73
priscilla <priscillacosta@id.uff.br>
2017-11-03 21:20:50 +0100

    model

--- tnm_guide/models.py
+++ tnm_guide/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+class Resource(models.Model):
+	MAKER_BRAND = 'MB'
+	EVENT = 'EV'
+	STORE = 'ST'
+	SPACE = 'SP'
+	CATEGORY_CHOICES = (
+		(MAKER_BRAND, 'Maker or Brand'),
+		(EVENT, 'Event'),
+		(STORE, 'Store'),
+		(SPACE, 'Space'),
+	)
+	name = models.CharField(max_length=100, blank=True)
+	url = models.URLField(blank=True)
+	local = models.CharField(max_length=250, blank=True)
+	category = models.CharField(max_length=100,
+				choices=CATEGORY_CHOICES,
+				default=MAKER_BRAND,
+				blank=True)
+
+	def __str__(self):
+ 		return self.name
+
+
 # Create your models here.
