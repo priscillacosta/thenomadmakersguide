2626da9718cb32600ce2fafd5559525c7973606a
priscilla <priscillacosta@id.uff.br>
2017-11-06 18:28:24 +0100

    for loop

--- tnm_guide/views.py
+++ tnm_guide/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Resource
 
 
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+	resources = Resource.objects.all()
+	return render(request, 'index.html', {'resources' : resources})
+
