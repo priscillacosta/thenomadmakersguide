a637a7f1e9b3fd95c2ca16520cc9894f3fc26b98
priscilla <priscillacosta@id.uff.br>
2017-11-08 00:41:37 +0100

    pagination

--- tnm_guide/views.py
+++ tnm_guide/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Resource
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def index(request):
-	resources = Resource.objects.all()
-	return render(request, 'index.html', {'resources' : resources})
+	resources_list = Resource.objects.all()
+	paginator = Paginator(resources_list, 24)
+	page = request.GET.get('page')
 
+	try:
+		resources = paginator.page(page)
+	except PageNotAnInteger:
+		resources = paginator.page(1)
+	except EmptyPage:
+		resources = paginator.page(paginator.num_pages)
+	return render(request, 'index.html', {'resources' : resources})
